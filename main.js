df8b792470df0c85b3fad744aa5febb0a0cab661
Vedang-7 <86293705+Vedang-7@users.noreply.github.com>
2022-06-12 16:10:48 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,15 @@
+function tp1(){
+    window.location="dog.html"  
+}
+function tp2(){
+    window.locate("");
+}
+function tp3(){
+    window.locate("");
+}
+function tp4(){
+    window.locate("");
+}
+function tp5(){
+    window.locate("");
+}
